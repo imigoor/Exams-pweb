36bb2ff8060de372280a3f14efa50b46d8d1dc5b
imigoor <imigorgm@gmail.com>
2025-05-06 21:54:34 -0300

    Ajustes no index.js e index.ts

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const Exam_1 = require("./Exam");
 const pesos = new Weight_1.Weight([2, 2, 2, 2, 2]);
 const gabarito = new Answer_1.Answer("Gabarito", ['a', 'b', 'a', 'c', 'd']);
 const prova = new Exam_1.Exam(gabarito, pesos);
-const aluno1 = new Answer_1.Answer("Aluno 1", ['a', 'b', 'b', 'b', 'b']); // acerta 2 questõe
-const aluno2 = new Answer_1.Answer("Aluno 2", ['a', 'b', 'a', 'c', 'd']); // acerta todas
-const aluno3 = new Answer_1.Answer("Aluno 2", ['a', 'b', 'c', 'c', 'd']);
-prova.add(aluno1);
-prova.add(aluno2);
-prova.add(aluno3);
+const aluno1Teste = new Answer_1.Answer("Aluno 1", ['a', 'b', 'b', 'b', 'b']);
+const aluno2Teste = new Answer_1.Answer("Aluno 2", ['a', 'b', 'a', 'c', 'd']);
+const aluno3Teste = new Answer_1.Answer("Aluno 2", ['a', 'b', 'c', 'c', 'd']);
+prova.add(aluno1Teste);
+prova.add(aluno2Teste);
+prova.add(aluno3Teste);
 console.log("Média:", prova.avg());
 console.log("Notas menores que 6:", prova.lt(6));
 console.log("Notas maiores que 6:", prova.gt(6));
--- index.ts
+++ index.ts
@@ -7,13 +7,13 @@ const gabarito = new Answer("Gabarito", ['a', 'b', 'a', 'c', 'd']);
 
 const prova = new Exam(gabarito, pesos);
 
-const aluno1 = new Answer("Aluno 1", ['a', 'b', 'b', 'b', 'b']); // acerta 2 questõe
-const aluno2 = new Answer("Aluno 2", ['a', 'b', 'a', 'c', 'd']); // acerta todas
-const aluno3 = new Answer("Aluno 2", ['a', 'b', 'c', 'c', 'd']);
+const aluno1Teste = new Answer("Aluno 1", ['a', 'b', 'b', 'b', 'b']);
+const aluno2Teste = new Answer("Aluno 2", ['a', 'b', 'a', 'c', 'd']); 
+const aluno3Teste = new Answer("Aluno 2", ['a', 'b', 'c', 'c', 'd']);
 
-prova.add(aluno1);
-prova.add(aluno2);
-prova.add(aluno3);
+prova.add(aluno1Teste);
+prova.add(aluno2Teste);
+prova.add(aluno3Teste);
 
 console.log("Média:", prova.avg());
 console.log("Notas menores que 6:", prova.lt(6));
